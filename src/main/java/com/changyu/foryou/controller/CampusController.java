8a1eb0e26039b39d7769ba47ee9036710bba53b5
l00251692 <982679298@qq.com>
2018-03-24 12:56:51 +0800

    删除无用的引用包

--- src/main/java/com/changyu/foryou/controller/CampusController.java
+++ src/main/java/com/changyu/foryou/controller/CampusController.java
@@ -36,7 +36,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Random;
 
-import javax.security.auth.message.callback.PrivateKeyCallback.IssuerSerialNumRequest;
+//import javax.security.auth.message.callback.PrivateKeyCallback.IssuerSerialNumRequest;
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
